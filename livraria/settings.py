39837c974680ce140b45748e9141a0c933088e93
-g <-g>
2022-11-04 21:18:59 -0500

    atualizando

--- livraria/settings.py
+++ livraria/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z&nowzs+zov=^oh)_a4iy+uza@sarmh$%5v!3$9f*t$0=o@xe('
+SECRET_KEY = 'django-insecure-d9BjnawADUxoU2sX6HTAnfNbbGDJQD-bG-g554hLMsk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
