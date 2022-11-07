4266a29250b88966fc3dc1ab4dd2e727c72cac6a
-g <-g>
2022-11-07 13:47:28 -0500

    cometando databases

--- livraria/settings.py
+++ livraria/settings.py
@@ -81,17 +81,17 @@ WSGI_APPLICATION = 'livraria.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'KauaneDelvoss',
-        'PASSWORD': 'kauanedelvoss',
-        'HOST': 'localhost',
-        'PORT': '',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'USER': 'KauaneDelvoss',
+#         'PASSWORD': 'kauanedelvoss',
+#         'HOST': 'localhost',
+#         'PORT': '',
 
-  }
-}
+#   }
+# }
 
 
 # Password validation
