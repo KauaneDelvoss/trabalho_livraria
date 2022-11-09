d781e96a0f7f2d98938db1b2fc1bfc995e6a2e21
-g <-g>
2022-11-08 20:52:24 -0500

    ajustes

--- core/models/usuario.py
+++ core/models/usuario.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 class Usuario(AbstractUser):
-    cpf = models.CharField(max_length=11, unique=True)
+    cpf = models.CharField(max_length=11, unique=True, blank=True, default=1)
     telefone = models.CharField(max_length=11, blank=True, null=True)
     data_nascimento = models.DateField(blank=True, null=True)
